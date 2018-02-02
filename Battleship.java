db4cf331359aed954357ce13ccd55cd3c343f1a0
yshristika <34748358+yshristika@users.noreply.github.com>
2018-02-01 19:18:47 -0500

    Update Battleship.java

--- Battleship.java
+++ Battleship.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Battleship {
+	/** Main class in theproject which handles the game. **/
 	public static void main(String args[])
 	{
 		int row,column;
@@ -10,24 +11,15 @@ public class Battleship {
 		System.out.println("Enter column");
 		column=input.nextInt();
 		
-		//BattleshipModel m1=new BattleshipModel(row,column);
-		//BattleshipModel m2=new BattleshipModel(row,column);
-		
 		BattleshipModel model=new BattleshipModel();
-        BattleshipView v1=new BattleshipView(row,column);
-        BattleshipView v2=new BattleshipView(row,column);
+        	BattleshipView v1=new BattleshipView(row,column);
+	        BattleshipView v2=new BattleshipView(row,column);
 		BattleshipController cont = new BattleshipController(model, v1, v2);
 		cont.createFleet();
 		cont.createTheFleet(0);
 		cont.createTheFleet(1);
 		cont.show();
 		cont.TheGame();
-		
-        
-		//BattleshipView view=new BattleshipView();
-        
-        
-        //BattleshipController cont = new BattleshipController(v1, v2, view);
 	}
 
 }
